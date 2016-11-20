1b206079922a9baa3fd14fead7bca5e256d25a91
melkir <melkir13@gmail.com>
2016-11-20 15:28:06 +0100

    (feature) accelerometer play

--- app/src/main/java/com/melkir/googlesamplesdemo/MainActivity.java
+++ app/src/main/java/com/melkir/googlesamplesdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.melkir.googlesamplesdemo.sensor.AccelerometerPlayActivity;
 import com.melkir.googlesamplesdemo.vision.BarcodeReaderActivity;
 import com.melkir.googlesamplesdemo.vision.FaceTrackerActivity;
 import com.melkir.googlesamplesdemo.vision.GooglyEyesActivity;
@@ -29,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void startGooglyEyesActivity(View view) {
         Intent intent = new Intent(this, GooglyEyesActivity.class);
+        startActivity(intent);
+    }
+
+    public void startAccelerometerPlayActivity(View view) {
+        Intent intent = new Intent(this, AccelerometerPlayActivity.class);
         startActivity(intent);
     }
 }
