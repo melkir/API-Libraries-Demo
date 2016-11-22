7a6d5d28da5be411d291afee893c1083eb5b6e3b
melkir <melkir13@gmail.com>
2016-11-22 17:05:08 +0100

    (refractor) avoid xml conflicts with the main class

--- material-design-codelab/src/main/java/com/melkir/materialdesigncodelab/MaterialDesignActivity.java
+++ material-design-codelab/src/main/java/com/melkir/materialdesigncodelab/MaterialDesignActivity.java
@@ -124,7 +124,7 @@ public class MaterialDesignActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.material_menu_main, menu);
         return true;
     }
 
